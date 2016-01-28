aa7e78ae513f962ce8a80744f8c841e2ed5a5286
Praakrit Pradhan <pradhapt@mail.uc.edu>
2016-01-28 10:33:48 -0500

    Just adding background.
    Because why not? Also displaying the sequence and the nth number.
    Wasn’t sure how pretty we wanted it, so BATMAN? i guess.

--- fibonacci.php
+++ fibonacci.php
@@ -1,27 +1,51 @@
 <html>
-<body>
-
-<h3> The nth Fibonacci number is:</h3>
-<?php
-$n = $_POST['n'];
-if(!empty($n)){
-	if ($n == 0){
-		$fib = 0;}
-	if ($n == 1 || $n == 2){
-		$fib = 1;}
-if ($n > 2){
-	$int1 =  1;
-	$int2 =  1;	
-	$fib = 0;
-   for($i = 1; $i < $n-2; $i++){
-      $fib = $int1 + $int2;
-      $int2 = $int1;
-      $int1 = $fib;
-   }}
-   echo $fib;
+<head>
+	<link rel="stylesheet" type="text/css" href="fibonacci.css" />
+</head>
+<style>
+body{
+background-image: url("bakground_image.jpg");
+background-size: 100%;
+background-position: fixed;
+background-repeat: no-repeat;
+background-color: black;
 }
-echo $_POST['n']
+@font-face {
+  font-family: BATMAN;
+       src:url(batman.woff);
+}
+</style>
+<body>
+<div id="divcenter">
+<h3> <font face='BATMAN' color='white'>The nth Fibonacci number is:</font></h3>
+<font color='white'>
+	<?php
+$n = $_POST['n'];
+	if ($n == 0){
+		$fib = 0;
+	}
+	if ($n == 1 || $n == 2){
+		$fib = 1;
+	}
+	if ($n >= 3){
+		$int1 =  1;
+		$int2 =  1;	
+		$fib = 0;
+		echo "The fibonacci sequence: <br>";
+		echo "1 1 ";
+	   for($i = 1; $i <= $n-2; $i++){
+	      $fib = $int1 + $int2;
+	      $int2 = $int1;
+	      $int1 = $fib;
+	      echo $fib;
+	      echo " ";
+	    }
+	}
+	echo "<br><br>And the nth number is: <br>";
+	echo $fib;
 ?>
+</font>
+</div>
 
 </body>
 </html>
--- index.php
+++ index.php
@@ -2,15 +2,31 @@
 <head>
 <link rel="stylesheet" type="text/css" href="fibonacci.css" />
 </head>
+<style>
+body{
+background-image: url("bakground_image.jpg");
+background-size: 100%;
+background-position: fixed;
+background-repeat: no-repeat;
+background-color: black;
+}
+@font-face {
+  font-family: BATMAN;
+       src:url(batman.woff);
+}
+</style>
 <body>
+
+
 <div id ="top_head">
-	<h1> Simple nth Fib Calculator </h1> 
+	<h1><font face='BATMAN' color='white' face=''>Simple nth Fib Calculator </font></h1> 
 </div>
-<div id="fib_form">
+	<div id="fib_form">
 		<form action="fibonacci.php" method="POST">
-		<label for="n">n = </label><input id="n" name = "n" value = "0" type="textbox" />
-		<br /><input type="submit" value="Calculate nth fibonacci number" />
-</div>
+		<label for="n"><font color='white'>Input N = </font></label><input id="n" name = "n" value = "0" type="textbox" />
+		<input padding-left="10px" type="submit" value="Calculate" />
+	</div>
+		
 </form>
 </body>
 </html>
