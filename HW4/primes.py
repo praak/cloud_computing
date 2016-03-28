1dfe77e1f73fd6377df4525036cb51a8c91636bc
Praakrit <praakritp@gmail.com>
2016-03-27 21:33:27 -0400

    Reduce is reduced

--- HW4/primes.py
+++ HW4/primes.py
@@ -1,26 +1,43 @@
 #!/usr/bin/env python
 import mincemeat
 import sys
+# import line_profiler
 
 # file = open(sys.argv[-1],'r')
-data = list(range(10000))
+data = list(range(100000))
 # file.close()
 datasource = dict(enumerate(data))
 # print("Data:")
 # print(datasource)
+# @profile
 
 def mapfn(k, v):
     import math
+    def ispali(v):
+        if (v != 0) or (v != 1):
+            num = str(v)
+            flag = 0
+            for ind in range(0,((len(num)/2)+1)):
+                    last = num[(len(num)-1)-ind]
+                    first =  num[ind]
+                    if last is not first:
+                        flag = 0
+                        break
+                    else:
+                        flag = 1
+        return flag
     # for n in range(1,20):
     # not needed for n range, as thats our data incoming.
     if (v is 2) or (v is 3):
-        yield 'Primes',int(v)
+        if ispali(v):
+            yield 'PaliPrimes',int(v)
     elif ((v % 2) is 0) or ((v % 3) is 0):
         return
     else:
         # n = int(v)
         flag = 1
         for i in (range(5,int(math.ceil(math.sqrt(v))))):
+        # for i in (range(5,((int((v)**(0.5))))+2)):
             if ((v % i) is 0):
                 flag = 0
                 break
@@ -29,25 +46,14 @@ def mapfn(k, v):
         if flag == 1:
             # print("Prime:")
             # print(v)
-            yield 'Primes',int(v)
+            if ispali(v):
+                yield 'PaliPrimes',int(v)
     #end else n
 #endmapfn
+# @profile
 
 def reducefn(k, vs):
-    # print(vs)
-    pali = vs[:]
-    for v in vs:
-        if (v == 0) or (v == 1):
-            pali.remove(v)
-        num = str(v)
-        for ind in range(0,((len(num)/2)+1)):
-            last = num[(len(num)-1)-ind]
-            first =  num[ind]
-            if last is not first:
-                pali.remove(v)
-                break
-
-    return pali
+    return vs
 
 s = mincemeat.Server()
 s.datasource = datasource
